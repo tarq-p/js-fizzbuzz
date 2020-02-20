9a96ce14c9e57ef5ff50200c0c068d2e56f4b304
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-02-20 16:18:39 +0100

    terzo esercizio js  senza attribuzione Fizz Buzz

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+function multiple(valor, multiple)
+
+{
+    resto = valor % multiple;
+
+    if(resto==0)
+        return true;
+    else
+        return false;
+}
+
+var multipli_3=[];
+var multipli_5=[];
+
+
+
+for(var i=1;i<=100;i++)
+
+{
+    if(multiple(i,3))
+        multipli_3.push(i);
+    if(multiple(i,5))
+        multipli_5.push(i);
+}
+
+document.write("Multipli di 3: ",multipli_3);
+document.write("<br>Multipli di 5: ",multipli_5);
