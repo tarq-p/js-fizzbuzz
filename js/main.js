6a83f0ade1b1318515ed1e82ad9a4f238835e54c
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-02-20 16:50:53 +0100

    aggiunta dei commenti da carta ad atom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Uso della funzione trovata su stackoverflow per il calcolo del numero multiplo usando il /
+// definizione dei multipli 3 e 5
+// creazione for di una lista di numeri da 0 a 100
+// definizione della condizione dei multipli
+// sostituire il multipli di 3 e 5 rispettivamente "fizz" e "buzz"
+// per i multipli sia di 3 che di 5 inserire fixxbuzz
+
 
 function multiple(valor, multiple)
 
@@ -11,7 +18,7 @@ function multiple(valor, multiple)
 }
 
 var multipli_3=[];
-var multipli_5=[];
+var multipli_fix=[];
 
 
 
@@ -22,6 +29,7 @@ for(var i=1;i<=100;i++)
         multipli_3.push(i);
     else if(multiple(i,5))
         multipli_5.push(i);
+        // console.log("fix");
 }
 
 document.write("Multipli di 3: ",multipli_3);
